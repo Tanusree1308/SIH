0130327accc13e37aa4598d60f910ef9743b740a
Tanusree <gaddamtanusree20@gmail.com>
2025-09-22 16:53:08 +0530

    change

--- backend/main.py
+++ backend/main.py
@@ -11,8 +11,8 @@ from slowapi.errors import RateLimitExceeded
 from slowapi import Limiter, _rate_limit_exceeded_handler
 from slowapi.util import get_remote_address
 
-from . import database, models, security, services
-from .services import analysis_service
+from backend import database, models, security
+from backend.services import analysis_service
 from .config import settings
 
 # --- App Initialization ---
